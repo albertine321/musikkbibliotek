82f8d063bf2517b358a12df168b78ff96292cd33
albertinemorkvikskjold08-lang <albertinemorkvikskjold08@gmail.com>
2026-02-10 12:39:12 +0100

    gjort klar frontend
    ble ferdig med å koble til frontend med backend slik at jeg kan begynne å jobbe på den. Jeg hadde en del problemer med å få koblet til backenden fordi problemet viste seg at react ikke var innstallert

--- backend/app.js
+++ backend/app.js
@@ -4,29 +4,26 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hei velkommen til mitt spillbiblotek!');
+  /*   res.send('Hei velkommen til mitt musikkbiblotek! Legg til /musikk for å se alle albumene i databasen'); */
 });
 
 //TODO: Lag et nytt GET-endepunkt som henter alle spill
-app.get('/spill', async (req, res) => {
-
-    console.log("Henter spill fra databasen...");
+app.get('/musikk', async (req, res) => {
+    console.log("Henter musikk fra databasen...");
     try {
         const data = await get_Album();
-        res.send(data);
+        res.json(data); // Bruk .json() i stedet for .send()
     } catch (error) {
-        console.log("Feil ved henting av spill");
-        res.send(error);
+        console.log("Feil ved henting av musikk");
+        res.status(500).json({ error: error.message }); // Send en riktig feilmelding
     }
 });
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
 
 //TODO: Lag et POST-endepunkt som legger til nye spill
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
